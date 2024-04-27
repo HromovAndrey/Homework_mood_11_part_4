19ca67035ac3f410b1c8ce1fbc8c22910226c02e
user <andrey.gromov.36912@gmail.com>
2024-04-27 09:49:44 +0300

    Домашня робота

--- Homework_part_4.py
+++ Homework_part_4.py
@@ -1,32 +1,33 @@
-# Завдання 2
-# Створіть клас для конвертування температури з Цельсія
-# у Фаренгейт, і навпаки. У класі має знаходитися два статичні
-# методи: для конвертування з Цельсія у Фаренгейт і для конвертування з Фаренгейта у Цельсій. Також клас має розрахувати
-# кількість підрахунків температури та повернути це значення
-# статичним методом.
-
-class TemperatureConverter:
-    conversion_count = 0
+# Завдання 3
+# Створіть клас для конвертування з метричної системи в
+# англійську, та навпаки. Реалізуйте функціональність у вигляді
+# статичних методів. Обов’язково реалізуйте конвертування
+# заходів довжини.
+class UnitConverter:
+    @staticmethod
+    def meters_to_feet(meters):
+        return meters * 3.28084
 
     @staticmethod
-    def celsius_to_fahrenheit(celsius):
-        TemperatureConverter.conversion_count += 1
-        return (celsius * 9/5) + 32
+    def feet_to_meters(feet):
+        return feet / 3.28084
 
-    @staticmethod
-    def fahrenheit_to_celsius(fahrenheit):
-        TemperatureConverter.conversion_count += 1
-        return (fahrenheit - 32) * 5/9
+meters_length = 10
+feet_length = UnitConverter.meters_to_feet(meters_length)
+print(f"{meters_length} метрів = {feet_length} футів")
 
-    @staticmethod
-    def get_conversion_count():
-        return TemperatureConverter.conversion_count
+converted_back = UnitConverter.feet_to_meters(feet_length)
+print(f"{feet_length} футів = {converted_back} метрів")
 
-celsius_temp = 25
-fahrenheit_temp = TemperatureConverter.celsius_to_fahrenheit(celsius_temp)
-print(f"{celsius_temp} градусів Цельсія = {fahrenheit_temp} градусів Фаренгейта")
 
-converted_back = TemperatureConverter.fahrenheit_to_celsius(fahrenheit_temp)
-print(f"{fahrenheit_temp} градусів Фаренгейта = {converted_back} градусів Цельсія")
-
-print(f"Кількість конвертацій: {TemperatureConverter.get_conversion_count()}")
+#class Bank:
+#     def __init__(self):
+#         self.balance = 10
+#
+#     @staticmethod
+#     def uah_to_usd(value_usd):
+#         return value_usd / 38
+#
+# object = Bank()
+# print(object.uah_to_usd(100))
+# print(Bank.uah_to_usd(2000))
